fb81ad20074953dc375d55ea2c9909bd1b4caeac
Júlio Dutra <jjcdutra@gmail.com>
2021-03-24 10:45:00 -0300

    first commit

---
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum;
+
+import io.jsonwebtoken.lang.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.isTrue(true);
+	}
+
+}
